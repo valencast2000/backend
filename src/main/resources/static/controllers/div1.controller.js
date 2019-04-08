9297b2886af5bf415c84e3b11ec8b2c2513d8d11
valencast2000 <valencast2000@gmail.com>
2019-04-08 20:44:05 -0300

    8/4/19 2

---
+++ src/main/resources/static/controllers/div1.controller.js
@@ -0,0 +1,5 @@
+var app=angular.module('frontend');
+app.controller('div1Controller',function($scope){
+	$scope.titulo="Hola Mundo";
+	$scope.numero=12;
+});
---
+++ src/main/resources/static/controllers/div2.controller.js
@@ -0,0 +1,4 @@
+var app=angular.module('frontend');
+app.controller('div2Controller',function($scope){
+	$scope.titulo="Hola Mundo desde controller 2";
+});
